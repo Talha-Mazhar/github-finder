8c8c903dde7e8877e82ecb2f1e016d5dac26690f
Talha-Mazhar <ahmadnutt9111@gmail.com>
2022-07-29 03:56:07 +0500

    Tailwind-Setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react'
-
-export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+function App() {
+  return (
+    <div className='bg-purple-500'>
+      <h1 className='text-xl'>Hello World</h1>
+      <button className='btn'>Click</button>
+    </div>
+  )
 }
+export default App
