5733b14075c055cef7dad81e73ecdbb5b6fd3efc
Talha-Mazhar <ahmadnutt9111@gmail.com>
2022-08-05 16:21:08 +0500

    lec9.10

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,4 @@
-import { createContext, PureComponent, useReducer } from 'react'
+import { createContext, useReducer } from 'react'
 import githubReducer from './GithubReducer'
 import React from 'react'
 
@@ -15,28 +15,41 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
-  //test Purposes
-  const fetchUsers = async () => {
+  const searchUsers = async (text) => {
     setLoading()
-    const response = await fetch(`${GITHUB_URL}/users`, {
+
+    const params = new URLSearchParams({
+      q: text,
+    })
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     })
 
-    const data = await response.json()
+    const { items } = await response.json()
 
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      payload: items,
     })
   }
 
+  const clearUsers = () =>
+    dispatch({
+      type: 'CLEAR_USERS',
+    })
   const setLoading = () => dispatch({ type: 'SET_LOADING' })
 
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import UserResults from '../components/users/UserResults'
+import UserSearch from '../components/users/UserSearch'
 function Home() {
   return (
-    <div>
-      <h1 className='text-6xl'>Welcome</h1>
+    <>
+      <UserSearch />
       <UserResults />
-      {/* //{process.env.REACT_APP_GITHUB_TOKEN} */}
-    </div>
+    </>
   )
 }
 
